4c0bcc9327c559bb6b152e3e2457a7f97275e1e9
Mikachu2333 <mikachu.23333@zohomail.com>
2024-04-28 23:41:33 +0800

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
+use arboard::Clipboard;
 use sha3::{Digest, Sha3_256};
-
 use std::fs::File;
 use std::io::Read;
 use std::path::PathBuf;
@@ -14,11 +14,13 @@ fn main() {
         panic!("Wrong file types.")
     }
 
-    calc_sha3_256(&input_path);
+    let name = input_path.file_name().unwrap().to_str().unwrap();
+    let sha3_256 = calc_sha3_256(&input_path);
+    let summary_info = format!("{} SHA3_256={}", name, sha3_256);
+    set_clipboard(&summary_info);
 }
 
-fn calc_sha3_256(file_path: &PathBuf) {
-    let name = file_path.file_name().unwrap().to_str().unwrap();
+fn calc_sha3_256(file_path: &PathBuf) -> String {
     let mut file = File::open(file_path).expect("无法打开文件");
     let mut buffer = Vec::new();
     file.read_to_end(&mut buffer).expect("无法读取文件内容");
@@ -27,5 +29,11 @@ fn calc_sha3_256(file_path: &PathBuf) {
     hasher.update(&buffer);
     let result = hasher.finalize();
 
-    println!("{} SHA3_256={:x}", name, result);
+    format!("{:x}", result)
+}
+
+fn set_clipboard(information: &String) {
+    let mut clipboard = Clipboard::new().expect("Error read clipboard.");
+    clipboard.set_text(&information.to_owned()).expect("Error write clipboard.");
+    println!("Success! {}",&information);
 }
